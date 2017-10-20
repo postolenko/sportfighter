82ec460d6587e3e09b4a71d86b549b21e38799b8
postolenko <denis.postolenko@gmail.com>
2017-10-20 06:25:35 +0300

    Sport Fighter

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/sportfighter/index.html
---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+
+    // var setFooterPositionInterval;
+    // var contentCoor;
+    // var footerCoor;
+
+    // ----------------------------
+
+    // getFooterPosition();
+
+    $(window).resize(function() {
+
+        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+        // if($(".footer").length > 0) {
+
+        //     $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+        // }
+
+    });
+
+
+    $(function() {
+
+        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+    });
+
+
+    // function getFooterPosition() {
+
+    //     $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+    //     if($(".footer").length > 0) {
+
+    //         $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    //         setFooterPositionInterval = setInterval(function() {
+
+    //             contentCoor = $(".content").offset().top + $(".content").height();
+    //             footerCoor = $(".footer").offset().top;
+
+    //             if( contentCoor != footerCoor ) {
+
+    //                 $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+    //                 $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    //                 clearInterval(setFooterPositionInterval);
+
+    //             }
+
+    //         }, 35);
+
+    //     }
+
+    // }
+
+
+
+});
